460c0bd147a0435735daa62a3b660671a6b452bb
NithinaMP <nithinampnithu@gmail.com>
2026-06-16 12:49:58 +0530

    Refactor application bootstrap for responsive architecture

--- lib/main.dart
+++ lib/main.dart
@@ -72,6 +72,9 @@
 //   }
 // }
 
+import 'dart:ui';
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
@@ -88,6 +91,17 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FlutterError.onError =
+      FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(
+      error,
+      stack,
+      fatal: true,
+    );
+    return true;
+  };
   runApp(const SuitcaseApp());
 }
 
